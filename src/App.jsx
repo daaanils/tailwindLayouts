992eb805c047f8fe2aec5c2e8f1de7a1d75a1c7d
Danilo <pelasod@gmail.com>
2024-12-01 12:05:36 +0800

    Exploring Tailwin CSS

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='m-4 grid gap-4 sm:grid-cols-4'>
+      <div className='min-h-[100px] rounded-lg shadow bg-orange-500'></div>
+      <div className='min-h-[100px] rounded-lg shadow bg-teal-500'></div>
+      <div className='min-h-[100px] rounded-lg shadow bg-teal-500'></div>
+      <div className='min-h-[100px] rounded-lg shadow bg-teal-500'></div>
+    </div>
+    </>
+  )
+}
+
+export default App
